755db8e314e097f32550db70890d3cbbc73b716b
Oleksandr Roskovynskyi <roskovynskyi@gmail.com>
2021-04-02 13:15:50 +0300

    Update JsonResponseTest.php

--- tests/Unit/Service/JsonResponseTest.php
+++ tests/Unit/Service/JsonResponseTest.php
@@ -7,16 +7,48 @@ use PHPUnit\Framework\TestCase;
 
 class JsonResponseTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testResponse()
+    public function testResponseInt()
     {
         $response = new JsonResponse(11);
 
         self::assertEquals('11', $response->getContent());
+        self::assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testResponseWithCode()
+    {
+        $response = new JsonResponse(11, 203);
+
+        self::assertEquals('11', $response->getContent());
+        self::assertEquals(203, $response->getStatusCode());
+    }
+
+    public function testResponseNull()
+    {
+        $response = new JsonResponse(null);
+
+        self::assertEquals('null', $response->getContent());
+        self::assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testResponseString()
+    {
+        $response = new JsonResponse('11');
+
+        self::assertEquals('"11"', $response->getContent());
+        self::assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testResponseObject()
+    {
+        $object = new \stdClass();
+        $object->string = '11';
+        $object->integer = 11;
+        $object->none = null;
+
+        $response = new JsonResponse($object);
+
+        self::assertEquals('{"string":"11","integer":11,"none":null}', $response->getContent());
         self::assertEquals(200, $response->getStatusCode());
     }
 }
